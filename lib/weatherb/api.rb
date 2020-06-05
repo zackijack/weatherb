b3f216990ab7b3e407ad037a3686eb96096a4812
Zackky Muhammad <m.zackky@gmail.com>
2020-06-05 11:05:06 +0700

    Add API to get nowcast Forecasting data on a minute-by-minute basis

--- lib/weatherb/api.rb
+++ lib/weatherb/api.rb
@@ -23,6 +23,7 @@ module Weatherb
     ].freeze
 
     DEFAULT_REALTIME_FIELDS = %w[
+      dewpoint
       wind_gust
       precipitation_type
       cloud_cover
@@ -30,6 +31,16 @@ module Weatherb
       cloud_ceiling
       surface_shortwave_radiation
       moon_phase
+    ].freeze
+
+    DEFAULT_NOWCAST_FIELDS = %w[
+      dewpoint
+      wind_gust
+      precipitation_type
+      cloud_cover
+      cloud_base
+      cloud_ceiling
+      surface_shortwave_radiation
     ].freeze
 
     SUCCESS_STATUSES = (200..300).freeze
@@ -51,14 +62,15 @@ module Weatherb
     end
 
     # Realtime (<= 1min out)
-    # The realtime call provides observational data at the present time,
-    # down to the minute, for a specific location. Information is available globally,
-    # with high-resolution data available for Japan Western Europe, India, and the US.
+    # The realtime call provides observational data at the present time, down
+    # to the minute, for a specific location. Information is available globally,
+    # with high-resolution data available for Japan Western Europe, India, and
+    # the US.
     #
     # @param lat [Float] Latitude, -59.9 to 59.9.
     # @param lon [Float] Longitude, -180 to 180.
     # @param unit_system [String] Unit system, "si" or "us".
-    # @param fields [Array] Selected fields from ClimaCell data layer (such as "precipitation" or "wind_gust").
+    # @param fields [Array<String>] Selected fields from ClimaCell data layer (such as "precipitation" or "wind_gust").
     #
     # @return [Hash]
     def realtime(lat:, lon:, unit_system: 'si', fields: DEFAULT_FIELDS | DEFAULT_REALTIME_FIELDS)
@@ -75,6 +87,43 @@ module Weatherb
       response = @connection.get(path, query)
       response_body(response)
     end
+
+    # Nowcast (<= 360min out)
+    # The nowcast call provides forecasting data on a minute-­by-­minute basis,
+    # based on ClimaCell’s proprietary sensing technology and models.
+    #
+    # 6 hours of nowcasting is available for the US. 3 hours of precipitation
+    # and 6 hours of non-precipitation data is provided for Japan and Western
+    # Europe.
+    #
+    # @param lat [Float] Latitude, -87 to 89.
+    # @param lon [Float] Longitude, -180 to 180.
+    # @param unit_system [String] Unit system, "si" or "us".
+    # @param timestep [Integer] Time step in minutes, 1-60.
+    # @param start_time [String] Start time in ISO 8601 format "2019-03-20T14:09:50Z", or "now".
+    # @param end_time [String] End time in ISO 8601 format "2019-03-20T14:09:50Z".
+    # @param fields [Array<String>] Selected fields from ClimaCell data layer (such as "precipitation" or "wind_gust").
+    #
+    # @return [Hash]
+    def nowcast(lat:, lon:, unit_system: 'si', timestep: 1, start_time: 'now', end_time: nil, fields: DEFAULT_FIELDS | DEFAULT_NOWCAST_FIELDS)
+      path = 'weather/nowcast'
+
+      query = {
+        apikey: @api_key,
+        lat: lat,
+        lon: lon,
+        unit_system: unit_system,
+        timestep: timestep,
+        start_time: start_time,
+        end_time: end_time,
+        fields: fields
+      }
+
+      response = @connection.get(path, query)
+      response_body(response)
+    end
+
+    private
 
     def response_body(response)
       if SUCCESS_STATUSES.member?(response.status)
