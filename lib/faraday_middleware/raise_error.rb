da7effacc7ce7b06fbd85a92eebc57f333a6dfbd
Zackky Muhammad <m.zackky@gmail.com>
2020-06-18 17:58:56 +0700

    Separating error messages with status code

--- lib/faraday_middleware/raise_error.rb
+++ lib/faraday_middleware/raise_error.rb
@@ -11,30 +11,26 @@ module FaradayMiddleware
       @app.call(env).on_complete do |response|
         case response[:status].to_i
         when 400
-          raise Weatherb::BadRequest.new(error_message(response), 400)
+          raise Weatherb::BadRequest.new(error_body(response[:body]), 400)
         when 401
-          raise Weatherb::Unauthorized.new(error_message(response), 401)
+          raise Weatherb::Unauthorized.new(error_body(response[:body]), 401)
         when 403
-          raise Weatherb::Forbidden.new(error_message(response), 403)
+          raise Weatherb::Forbidden.new(error_body(response[:body]), 403)
         when 429
-          raise Weatherb::TooManyRequests.new(error_message(response), 429)
+          raise Weatherb::TooManyRequests.new(error_body(response[:body]), 429)
         when 500
-          raise Weatherb::InternalServerError.new(error_message(response), 500)
+          raise Weatherb::InternalServerError.new(error_body(response[:body]), 500)
         when CLIENT_ERROR_STATUSES
-          raise Weatherb::ClientError.new(error_message(response), 400)
+          raise Weatherb::ClientError.new(error_body(response[:body]), 400)
         when SERVER_ERROR_STATUSES
-          raise Weatherb::ServerError.new(error_message(response), 500)
+          raise Weatherb::ServerError.new(error_body(response[:body]), 500)
         when nil
-          raise Weatherb::NilStatusError.new(error_message(response), 0)
+          raise Weatherb::NilStatusError.new(error_body(response[:body]), 0)
         end
       end
     end
 
     private
-
-    def error_message(response)
-      "status: #{response[:status]}, message: #{error_body(response[:body])}"
-    end
 
     def error_body(body)
       body = ::JSON.parse(body) if !body.nil? && !body.empty? && body.is_a?(String)
